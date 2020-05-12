c336a9d34944aa3ae3355e719f93af835fc5d392
Luke Parker <luke.parker9@tafensw.edu.au>
2020-05-12 13:04:56 +1000

    Added Daily Challenge 1

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,27 @@
- 
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function askQuestion(question){
+    let answer;
+
+    return new Promise((resolve, reject) => {
+        rl.question(question, (ans) => {
+            resolve(ans);
+        })
+    });
+}
+
+
+async function Program(){
+    // Your Code Goes Here...
+    let username = await askQuestion("What is your name?\n");
+    console.log("Hello " + username);
+}
+
+
+Program().then(() => {
+    process.exit(0);
+});
